3c9614a53bdc9d7518b91cf16ca3bd5ed58828c0
monicapuisoru <elsene.scarlett@yahoo.com>
2015-05-12 17:49:19 +0300

    first C# course (5 ex)

--- TestExercitii/Program.cs
+++ TestExercitii/Program.cs
@@ -8,6 +8,8 @@ namespace TestExercitii
 {
     class Program
     {
+        public enum Seasons { summer, autumn, spring, winter };
+
 
         static Boolean checkDate(DayOfWeek day)
         {
@@ -17,7 +19,6 @@ namespace TestExercitii
             if ((myDay.ToUpper()).Equals(sunday.ToUpper()) || myDay.ToUpper().Equals(saturday.ToUpper()))
             {
                 Console.WriteLine("true");
-                Console.ReadLine();
                 return true;
             }
             else {
@@ -27,10 +28,97 @@ namespace TestExercitii
             }
         }
 
-        static void Main(string[] args)
+
+        static void printSeasons(Seasons mySeasons)
         {
-             DateTime dt = new DateTime(2015, 5, 16);
+          
+            switch (mySeasons)
+            { 
+                case Seasons.summer:
+                    Console.WriteLine("Summer!");
+                    break;
+               case Seasons.autumn:
+                    Console.WriteLine("Autumn!");
+                    break;
+                case Seasons.winter:
+                    Console.WriteLine("Winter!");
+                    break;
+                case Seasons.spring:
+                    Console.WriteLine("Spring!");
+                    break;
+                default:
+                    Console.WriteLine("Not a season!");
+                    break;
+            }
+        }
+
+        static string country(String[] countries)
+        {
+            int max = 0;
+            String name = "";
+            
+            foreach(String country in countries)
+            {
+                int nr = country.Length;
+                if (nr > max)
+                {
+                    max = nr;
+                    name = country.ToString();
+                }
+            }
+            return name;
+        }
+
+        static string countryParams(params String[] countries)
+        {
+            int max = 0;
+            String name = "";
+
+            foreach (String country in countries)
+            {
+                int nr = country.Length;
+                if (nr > max)
+                {
+                    max = nr;
+                    name = country.ToString();
+                }
+            }
+            return name;
+        }
+
+
+        static void printPrice(double price, int age, out double priceWithDisc)
+        {
+            if (age < 7)
+                priceWithDisc = price - 25 / 100 * price;
+            else if (age <= 14 && age >= 7)
+                priceWithDisc = price - 15 / 100 * price;
+            else 
+                priceWithDisc = price - 5 / 100 * price;
+
+            
+        }
+
+        static void Main(string[] args)
+        {/*
+            DateTime dt = new DateTime(2015, 5, 16);
              Boolean ans = checkDate(dt.DayOfWeek);
+            
+            Seasons s = Seasons.spring;
+            printSeasons(s);
+
+            string[] countries = new string[] { "Romania", "England", "France", "United States of America" };
+       
+            String name = countryParams("Franta", "Romania", "Spania", "Italia", "Norvegia");
+            Console.WriteLine(name);
+            Console.ReadLine();
+            */
+            double price = 100.0;
+            int age = 10;
+            double priceWithDisc;
+            printPrice(price, age, out priceWithDisc);
+            Console.WriteLine(priceWithDisc);
+            Console.ReadLine();
         }
     }
 }

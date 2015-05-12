e9977a1fa2e6357075ddc0a308ec6241c03798c7
monicapuisoru <elsene.scarlett@yahoo.com>
2015-05-12 16:51:42 +0300

    Day of the week commit

---
+++ TestExercitii/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExercitii
+{
+    class Program
+    {
+
+        static Boolean checkDate(DayOfWeek day)
+        {
+            string saturday = "saturday";
+            string sunday = "sunday";
+            string myDay = day.ToString();
+            if ((myDay.ToUpper()).Equals(sunday.ToUpper()) || myDay.ToUpper().Equals(saturday.ToUpper()))
+            {
+                Console.WriteLine("true");
+                Console.ReadLine();
+                return true;
+            }
+            else {
+                Console.WriteLine("false");
+                Console.ReadLine();
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+             DateTime dt = new DateTime(2015, 5, 16);
+             Boolean ans = checkDate(dt.DayOfWeek);
+        }
+    }
+}

126dd33e1015273c993daff6a0b3ba6fb25c3d3c
monicapuisoru <elsene.scarlett@yahoo.com>
2015-05-13 10:16:02 +0300

    First C# course - complete

--- TestExercitii/Program.cs
+++ TestExercitii/Program.cs
@@ -9,7 +9,7 @@ namespace TestExercitii
     class Program
     {
         public enum Seasons { summer, autumn, spring, winter };
-
+        public enum DiscountTypes { General, Promotion, BestDeal };
 
         static Boolean checkDate(DayOfWeek day)
         {
@@ -90,14 +90,75 @@ namespace TestExercitii
         static void printPrice(double price, int age, out double priceWithDisc)
         {
             if (age < 7)
-                priceWithDisc = price - 25 / 100 * price;
+                priceWithDisc = price - 25.0 / 100.0 * price;
             else if (age <= 14 && age >= 7)
-                priceWithDisc = price - 15 / 100 * price;
+                priceWithDisc = price - 15.0 / 100.0 * price;
             else 
-                priceWithDisc = price - 5 / 100 * price;
-
-            
+                priceWithDisc = price - 5.0 / 100.0 * price;
         }
+
+
+
+        static void printPrice2(ref double price, int age)
+        {
+            if (age < 7)
+                price = price - 25.0 / 100.0 * price;
+            else if (age <= 14 && age >= 7)
+                price = price - 15.0 / 100.0 * price;
+            else
+                price = price - 5.0 / 100.0 * price;
+        }
+
+
+        static double extraDiscount(double price, int age  = 14, DiscountTypes tip = DiscountTypes.Promotion)
+        {
+            double newPrice = price;
+            if (age < 7)
+                newPrice = price - 25.0 / 100.0 * price;
+            else if (age <= 14 && age >= 7)
+                newPrice = price - 15.0 / 100.0 * price;
+            else
+                newPrice = price - 5.0 / 100.0 * price;
+
+            switch (tip)
+            {
+                case DiscountTypes.BestDeal:
+                    newPrice = price - 50.0 / 100.0 * price;
+                    break;
+               
+                case DiscountTypes.Promotion:
+                    newPrice = price - 25.0 / 100.0 * price;
+                    break;
+                default:
+                    break;
+            }
+            return newPrice;
+        }
+        static double extraDiscount(int price, int age = 14, DiscountTypes tip = DiscountTypes.Promotion)
+        {
+            double newPrice = (double)price;
+            if (age < 7)
+                newPrice = (double)price - 25.0 / 100.0 * (double)price;
+            else if (age <= 14 && age >= 7)
+                newPrice = (double)price - 15.0 / 100.0 * (double)price;
+            else
+                newPrice = (double)price - 5.0 / 100.0 * (double)price;
+
+            switch (tip)
+            {
+                case DiscountTypes.BestDeal:
+                    newPrice = (double)price - 50.0 / 100.0 * (double)price;
+                    break;
+
+                case DiscountTypes.Promotion:
+                    newPrice = (double)price - 25.0 / 100.0 * (double)price;
+                    break;
+                default:
+                    break;
+            }
+            return newPrice;
+        }
+
 
         static void Main(string[] args)
         {/*
@@ -113,11 +174,29 @@ namespace TestExercitii
             Console.WriteLine(name);
             Console.ReadLine();
             */
-            double price = 100.0;
-            int age = 10;
-            double priceWithDisc;
-            printPrice(price, age, out priceWithDisc);
-            Console.WriteLine(priceWithDisc);
+            double price = 98.0;
+            int age = 5;
+            //double priceWithDisc;
+           // printPrice(price, age, out priceWithDisc);
+            Console.WriteLine(price);
+            printPrice2(ref price, age);
+          //  Console.WriteLine(priceWithDisc);
+          //  Console.WriteLine(price);
+           // Console.ReadLine();
+            double rez;
+            //rez = extraDiscount(price, 5, DiscountTypes.Promotion);
+           // Console.WriteLine(rez);
+
+            rez = extraDiscount(price);
+            Console.WriteLine(rez);
+            rez = extraDiscount(price, tip : DiscountTypes.Promotion);
+            Console.WriteLine(rez);
+            rez = extraDiscount(price, tip: DiscountTypes.Promotion, age : 5);
+           Console.WriteLine(rez);
+            rez = extraDiscount(100, 5, DiscountTypes.Promotion);
+
+
+            Console.WriteLine("rez overload:  " + rez);
             Console.ReadLine();
         }
     }
